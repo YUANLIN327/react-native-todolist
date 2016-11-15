f94b2483cf0a8c118108bd12d224664d6f8062fa
Lin Yuan <yuanlin.dalian@gmail.com>
2016-11-15 16:13:20 -0500

    Initial commit.

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,7 @@
  */
 
 import React, {Component} from 'react';
+import App from './app/App';
 import {
     AppRegistry,
     StyleSheet,
@@ -13,213 +14,37 @@ import {
     TextInput,
     Dimensions,
     TouchableOpacity,
-    ListView
+    ListView,
+    Navigator
 } from 'react-native';
-import _ from 'lodash';
-import {observable, autorun} from 'mobx';
 
-const {height, width} = Dimensions.get('window');
+class TodoList extends Component {
+   renderScene (route, navigator) {
+       return <route.component {...route.passProps} navigator ={navigator} />
+   }
 
-class ToDoInput extends Component {
-    add() {
-        this.textInput.focus();
-        return this.props.addTodo();
+    configureScene(route, routeStack) {
+        if (route.type === 'Modal') {
+            return Navigator.SceneConfigs.FloatFromBottom;
+        }
+        return Navigator.SceneConfigs.PushFromRight;
     }
 
     render() {
         return (
-            <View style={styles.textInputContainer}>
-                <TextInput
-                    ref={(input) => this.textInput = input}
-                    placeholder='What are you up to...'
-                    style={styles.textInput}
-                    onChangeText={this.props.onChangeText}
-                    value={this.props.inputText}
-                />
-                <TouchableOpacity onPress={this.add.bind(this)}>
-                    <View style={styles.button}>
-                        <Text style={styles.buttonText}>+</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-}
-
-class ToDoItem extends Component {
-    renderCheck(checked) {
-        if (checked) {
-            return (
-                <View style={[styles.iconContainer, {backgroundColor: 'grey', borderWidth: 1, borderColor: 'black'}]}>
-                    <Text style={styles.icon}>✔️</Text>
-                </View>
-            );
-        }
-        return (
-            <View style={[styles.iconContainer,
-                {backgroundColor: 'white', borderWidth: 1, borderColor: 'black'}]}></View>
-        );
-    }
-
-    renderText(checked) {
-        if (!checked) {
-            return (
-                <Text
-                    style={styles.listItemText}>{parseInt(this.props.rowID) + 1}.&nbsp;{this.props.toDoItem.text}</Text>
-            );
-        }
-        return (
-            <Text
-                style={[styles.listItemText, {textDecorationLine: 'line-through'}]}>{parseInt(this.props.rowID) + 1}.&nbsp;{this.props.toDoItem.text}</Text>
-        );
-    }
-
-    deleteSelf() {
-        return this.props.delete(this.props.rowID);
-    }
-
-    render() {
-        return (
-            <View style={styles.itemRow}>
-                <TouchableOpacity onPress={() => this.props.check(this.props.rowID)}>
-                    {this.renderCheck(this.props.toDoItem.isDone)}
-                </TouchableOpacity>
-                {this.renderText(this.props.toDoItem.isDone)}
-                <TouchableOpacity onPress={this.deleteSelf.bind(this)}>
-                    <View style={styles.iconContainer}>
-                        <Text style={styles.icon}>✖️</Text>
-                    </View>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-}
-
-
-export default class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            inputText: '',
-            items: [{text: 'firstTodo', isDone: false}],
-            rows: ds.cloneWithRows([])
-        }
-    }
-
-    addTodo() {
-        const toDoItem = {
-            text: this.state.inputText,
-            isDone: false
-        };
-        const items = this.state.items.concat(toDoItem);
-        this.setState({inputText: '', items});
-    }
-
-    getDataSource() {
-        return this.state.rows.cloneWithRows(this.state.items);
-    }
-
-    onChangeText(inputText) {
-        this.setState({inputText})
-    }
-
-    delete(rowID) {
-        const items = this.state.items;
-        items.splice(rowID, 1);
-        this.setState({items});
-    }
-
-    check(index) {
-        const items = this.state.items;
-        items[index].isDone = !items[index].isDone;
-        this.setState({items});
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    ToDo List
-                </Text>
-                <ToDoInput ref={(input) =>this.textInput = input}
-                           inputText={this.state.inputText}
-                           onChangeText={this.onChangeText.bind(this)}
-                           addTodo={this.addTodo.bind(this)}
-                />
-                <ListView
-                    dataSource={this.getDataSource()}
-                    renderRow={(rowData, sectionID, rowID) =>
-                        <ToDoItem toDoItem={rowData} rowID={rowID}
-                                  delete={this.delete.bind(this)} check={this.check.bind(this)}/>
+            <Navigator
+                configureScene={this.configureScene.bind(this)}
+                renderScene={this.renderScene.bind(this)}
+                initialRoute={{
+                    component: App,
+                    passProps: {
+                        store: ListStore
                     }
-                />
-            </View>
-        );
+                }}
+            />
+        )
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-        paddingTop: 40
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        marginBottom: 20
-    },
-    textInputContainer: {
-        alignSelf: 'stretch',
-        flexDirection: 'row',
-        alignItems: 'flex-start',
-        justifyContent: 'space-around'
-    },
-    textInput: {
-        paddingLeft: 12,
-        marginBottom: 20,
-        height: 48,
-        width: width * 0.7,
-        borderWidth: 1
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    button: {
-        height: 48,
-        width: 48,
-        backgroundColor: 'gray',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    buttonText: {
-        fontSize: 36,
-        color: 'white'
-    },
-    itemRow: {
-        flexDirection: 'row',
-        width: width * 0.9,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginTop: 20
-    },
-    listItemText: {
-        fontSize: 18
-    },
-    iconContainer: {
-        height: 24,
-        width: 24,
-        borderRadius: 8,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    icon: {fontSize: 12}
-});
 
 AppRegistry.registerComponent('TodoList', () => TodoList);
